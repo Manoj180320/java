53a0756ffb53b904d75d14f52681f94d23a6ed2d
Manoj180320 <151818575+Manoj180320@users.noreply.github.com>
2024-04-02 17:52:39 +0530

    Update Helloworld.java
    updated in git ui

--- src/com/company/Helloworld.java
+++ src/com/company/Helloworld.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git, How are you");
     }
 }
